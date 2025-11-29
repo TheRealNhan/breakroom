34c7e5ca237a11b67352e1e6bfc753191a50b55d
Bùi Nguyễn Minh Nhân <minhnhanbuinguyen@gmail.com>
2025-11-29 22:44:16 +0700

    modify series of files

---
+++ src/App.tsx
@@ -0,0 +1,11 @@
+import './styles/index.css'
+import { RageRoom } from './components/RageRoom'
+import './styles/App.css';
+
+function App() {
+  return (
+    <RageRoom />
+  );
+}
+
+export default App;
